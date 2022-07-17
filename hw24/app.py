e52d188323ce06c9aea4e88620ad03085403c714
Admin <Dexter2366@yandex.ru>
2022-07-17 13:57:34 +0300

    HW_24

---
+++ hw24/app.py
@@ -0,0 +1,37 @@
+import os
+from utils import build_query
+
+from flask import Flask, request, Response
+from flask_restx import abort
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+
+@app.route("/perform_query", methods=['POST', 'GET'])
+def perform_query() -> Response:
+    cmd_1 = request.args.get('cmd_1')
+    val_1 = request.args.get('val_1')
+    cmd_2 = request.args.get('cmd_2')
+    val_2 = request.args.get('val_2')
+    file_name = request.args.get('file_name')
+
+    if not(cmd_1 and val_1 and file_name):
+        abort(400)
+
+    file_path = os.path.join(DATA_DIR, str(file_name))
+    if not os.path.exists(file_path):
+        return abort(400, 'Wrong filepath')
+
+    with open(file_path) as file:
+        res = build_query(str(cmd_1), str(val_1), file)
+        if cmd_2 and val_2:
+            res = build_query(str(cmd_2), str(val_2), iter(res))
+
+    return app.response_class('\n'.join(res), content_type="text/plain")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ hw24/utils.py
@@ -0,0 +1,26 @@
+import re
+from typing import Iterator, List, Any
+
+
+def build_query(cmd: str, val: str, file_list: Iterator) -> List[Any]:
+    if cmd == 'filter':
+        return list(filter(lambda x: val in x, file_list))
+
+    if cmd == 'map':
+        return list([x.split()[int(val)] for x in file_list])
+
+    if cmd == 'unique':
+        return list(set(file_list))
+
+    if cmd == 'sort':
+        reverse = val == 'disc'
+        return list(sorted(file_list, reverse=reverse))
+
+    if cmd == 'limit':
+        val = int(val)
+        return list(file_list)[:int(val)]
+    if cmd == 'regex':
+        regex = re.compile(val)
+        return list(filter(lambda  x: regex.search(x), file_list))
+    return []
+
